0e9db0c1b0940d94f1bb4ad855bd1732ddb523fa
Iasaldo Frota <iyosaldonbk@gmail.com>
2025-02-19 14:17:41 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,10 +3,6 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.get("/",(req, res) => {
-  res.send("Hello World");
-});
-
 
 app.listen(PORT, () => {
   console.log("Hello World");
